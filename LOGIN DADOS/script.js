88a5e87cdb8dd7d763cea2a849492b3b5aa8d582
bernardo-ds <bernardomoto8@gmail.com>
2025-11-04 17:15:23 -0300

    Add files via upload
    LOGIN COM BANCO DE DADOS

---
+++ LOGIN DADOS/script.js
@@ -0,0 +1,34 @@
+function cadastrar(){
+    const usuario =  document.getElementById('usuario').value
+    const senha = document.getElementById('senha').value
+    const confirmarSenha = document.getElementById('confirmarSenha').value
+
+
+if(usuario && senha === confirmarSenha){
+    localStorage.setItem(usuario,senha)
+    return alert(`Usuário ${usuario} cadastrado com sucesso!`)
+}
+else{
+    return alert("Usuário e/ou senha incorretos")
+}
+}
+
+function login(){
+    const usuario = document.getElementById('usuario').value
+    const senha = document.getElementById('senha').value
+
+    let usuarioExistente = localStorage.getItem(usuario)
+
+    if(!usuarioExistente){
+        return alert("Usuário não existe!!!")
+    }
+
+    if(usuario && senha === usuarioExistente){
+        localStorage.setItem(usuario,senha)
+        alert(`Usuário ${usuario} logado com sucesso`)
+        window.location.href = "./home.html"
+    }
+    else{
+        return alert("Usuário e/ou senha incorretos")
+    }
+}
