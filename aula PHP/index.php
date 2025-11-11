79b9b994a0e85891de46e59354f8dfa46820db55
bernardo-ds <bernardomoto8@gmail.com>
2025-11-11 17:02:49 -0300

    Add files via upload
    php

---
+++ aula PHP/index.php
@@ -0,0 +1,5 @@
+<form action="connection.php">
+    <input type="text" placeholder="usuario">
+    <input type="password" placeholder="senha">
+    <input type="submit" placeholder="logar">
+</form>
