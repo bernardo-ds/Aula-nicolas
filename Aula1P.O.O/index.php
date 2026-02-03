abe44c589a07e41561f3edebe2b1107747ec41d9
bernardo-ds <bernardomoto8@gmail.com>
2026-02-03 17:07:52 -0300

    Add files via upload

--- Aula1P.O.O/index.php
+++ Aula1P.O.O/index.php
@@ -1,29 +1,109 @@
 <?php
 
-    class Pessoa{
-        public $nome;
-        public $altura;
-        public $peso;
-        public $cpf;
+    class Automovel{
+        public $modelo;
+        public $ano;
+        public $motor;
 
-        function __construct(string $nome, float $altura, float $peso, string $cpf)
+        function __construct(string $modelo, float $ano, string $motor)
         {
-            $this->nome = $nome;
-            $this->altura = $altura;
-            $this->peso = $peso;
-            $this->cpf = $cpf;
+            $this->modelo = $modelo;
+            $this->ano = $ano;
+            $this->motor = $motor;
         }
 
-        function andar(){
-            echo "{$this->nome} ta andando \n";
+        function dirigir(){
+            echo "{$this->modelo} ta andando <br>";
+        }
+        function virar(){
+            echo "{$this->modelo} ta virando <br>";
+        }
+        function tras(){
+            echo "{$this->modelo} ta indo para trás <br> <br> <br>";
         }
     }
 
-    $joao = new Pessoa("João", 1.76, 70, "123345567890");
 
-    $joao->andar();
+    $Jetta_GLI = new Automovel("Jetta GLI", 2022, "Abg23");
+
+    $Jetta_GLI->dirigir();
+    $Jetta_GLI->virar();
+    $Jetta_GLI->tras();
 
 
-    $japa = new Pessoa("Japa", 1.23, 2, "123345567890");
+    $XJ6 = new Automovel("XJ6", 2015, "Abg24");
+    
+    $XJ6->dirigir();
+    $XJ6->virar();
+    $XJ6->tras();
+    $ONIBUS = new Automovel("ONIBUS", 2002, "Abg25");
+    
+    $ONIBUS->dirigir();
+    $ONIBUS->virar();
+    $ONIBUS->tras();
 
-    $japa->andar();
+    class Animal{
+        public $especie;
+        public $idade;
+        public $peso;
+
+        function __construct(string $especie, float $idade, float $peso)
+        {
+            $this->especie = $especie;
+            $this->idade = $idade;
+            $this->peso = $peso;
+        }
+        function andar(){
+            echo "{$this->especie} esta andando <br>";
+        }
+        function correndo(){
+            echo "{$this->especie} esta correndo <br>";
+        }
+        function comendo(){
+            echo "{$this->especie} esta comendo <br> <br> <br>";
+        }
+    }
+
+    $Vaca = new Animal("Vaca", 12, 120);
+
+    $Vaca->andar();
+    $Vaca->correndo();
+    $Vaca->comendo();
+
+    $cachorro = new Animal("cachorro", 10, 23);
+
+    $cachorro->andar();
+    $cachorro->correndo();
+    $cachorro->comendo();
+
+    $passaro = new Animal("passaro", 4, 2);
+
+    $passaro->andar();
+    $passaro->correndo();
+    $passaro->comendo();
+
+    class Filme{
+        public $genero;
+        public $ano;
+        public $nome;
+
+        function __construct(string $genero, float $ano, string $nome)
+        {
+            $this->genero = $genero;
+            $this->ano = $ano;
+            $this->nome = $nome;
+        }
+        function filme(){
+            echo "O filme {$this->nome} esta sendo assistido <br>";
+        }
+    }
+
+    $homem_aranha =  new Filme("ficção cientifica", 2020, "homem aranha");
+    $homem_aranha->filme();
+
+    $Batman =  new Filme("ação", 2022, "Batman");
+    $Batman->filme();
+
+    $Um_Sonho_de_Liberdade =  new Filme("Drama", 2022, "Um Sonho de Liberdade");
+    $Um_Sonho_de_Liberdade->filme();
+
